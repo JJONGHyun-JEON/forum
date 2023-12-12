39b2116b4ca30dbab7803e25bd1548500212611b
JJONGHyun-JEON <wjswhdgus153@gmail.com>
2023-12-12 17:33:47 +0900

    댓글기능완성해야함

---
+++ pages/api/post/edit.js
@@ -0,0 +1,14 @@
+import { connectDB } from "@/util/database"
+import { MongoClient } from "mongodb"
+
+export default async function handler(요청, 응답){
+    if(요청.method == 'POST'){
+        
+        const db = (await connectDB).db('forum') //DB입출력 코드는 server component 안에서만 사용
+        let result = await db.collection('post').updateOne(
+            {},{$set : {수정}}
+        ) //post컬렉션에 있는 모든 데이터를 꺼내주세요
+        return 응답.status(200).redirect('/list')
+    }
+    
+}
